0c11d41bd8057b361649bbf6e1dbeb5d3c8a783e
Mikhail <plohov.mm@mail.ru>
2020-05-11 21:47:50 +0500

    Личный проект. Раздел 5. Завершена вёрстка старницы каталога

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,22 @@ navToggle.addEventListener('click', () => {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+const catalogOrderButtons = document.querySelectorAll('.catalog__button');
+const modal = document.querySelector('.modal');
+
+for (let i = 0; i < catalogOrderButtons.length; i++) {
+  const currentButton = catalogOrderButtons[i];
+  currentButton.addEventListener('click', () => {
+    modal.classList.remove('modal--close');
+    modal.classList.add('modal--open');
+  });
+}
+
+const modalButton = document.querySelector('.modal__button');
+modalButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  modal.classList.remove('modal--open');
+  modal.classList.add('modal--close');
+})
